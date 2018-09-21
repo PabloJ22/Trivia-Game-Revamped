f178a4267aa41621f1073c5a036e4ac170f2f293
APabloJ <altisimop@gmail.com>
2018-09-21 14:32:39 -0500

    added 'questions' array in app.js file.

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+let questions = [{
+        question: "what is the outcome of: 236 x 2 - 50 - 2 =_____?",
+        answers: [580, 408, 420, 460],
+        correctAnswer: 420
+    }, {
+        question: "What is the average lifespan of a sloth?",
+        answers: ["eternal", "I Love sloths <3", "10 - 20 years", "20 - 30 years"],
+        correctAnswer: "20 - 30 years"
+    },
+    {
+        question: "Which of these animals CANNOT reproduce body parts?",
+        answers: ["Tunicate", "Star Fish", "Axolotl", "White-tailed Deer"],
+        correctAnswer: ["Tunicate", "Star Fish", "Axolotl", "White-tailed Deer"]
+    },
+    {
+        question: "Which country consumes the most alcohol per capita?",
+        answers: ["Belarus", "Lithuania", "Russia", "Romania"],
+        correctAnswer: "Belarus"
+    },
+    {
+        question: "How many eggs does a chicken lay in a lifetime?",
+        answers: [530, 430, 330, 230],
+        correctAnswer: 430
+
+    }
+];
